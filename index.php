a24fc77969e1ef91c0d7d95a42add3dc1f267002
Chaitali1982 <67077628+Chaitali1982@users.noreply.github.com>
2020-10-27 15:51:37 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
 		    <!-- Load my style -->
       <link rel="stylesheet" href="lib/css/style.css">
+      <link rel="shortcut icon" href="#" />
       <!-- Load Bootstrap -->
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
